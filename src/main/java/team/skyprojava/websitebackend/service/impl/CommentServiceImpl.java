b20c552db23da1be10edc448d86fb7a8f6dd15ff
Vyacheslav <viacheslav87klerik@gmail.com>
2023-04-25 12:52:20 +0300

    GET запросы, исправлены ошибки в AdsController-3. Добавлено все остальное.

--- src/main/java/team/skyprojava/websitebackend/service/impl/CommentServiceImpl.java
+++ src/main/java/team/skyprojava/websitebackend/service/impl/CommentServiceImpl.java
@@ -2,18 +2,26 @@ package team.skyprojava.websitebackend.service.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import team.skyprojava.websitebackend.dto.AdsDto;
 import team.skyprojava.websitebackend.dto.CommentDto;
+import team.skyprojava.websitebackend.dto.ResponseWrapperAdsDto;
 import team.skyprojava.websitebackend.dto.ResponseWrapperCommentDto;
+import team.skyprojava.websitebackend.entity.Ads;
+import team.skyprojava.websitebackend.entity.Comment;
+import team.skyprojava.websitebackend.exception.AdsNotFoundException;
 import team.skyprojava.websitebackend.mapper.CommentMapper;
 import team.skyprojava.websitebackend.repository.AdsCommentRepository;
 import team.skyprojava.websitebackend.service.CommentService;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CommentServiceImpl implements CommentService {
     Logger logger = LoggerFactory.getLogger(CommentService.class);
     AdsCommentRepository adsCommentRepository;
     CommentMapper commentMapper;
+
     @Override
     public ResponseWrapperCommentDto getAdsComments(int id) {
         logger.info("Was invoked method for get ads comment by id");
