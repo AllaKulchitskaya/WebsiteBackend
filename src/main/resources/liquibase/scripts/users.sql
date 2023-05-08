426a649cc6b1a6601489f14c8a3075b376baecad
AllaKulchitskaya <alla070@list.ru>
2023-05-08 12:55:33 +0300

    изменения в таблицах бд

--- src/main/resources/liquibase/scripts/users.sql
+++ src/main/resources/liquibase/scripts/users.sql
@@ -17,3 +17,15 @@ CREATE TABLE users
 --changeset alla:2
 ALTER TABLE users
     ADD FOREIGN KEY (user_image_id) REFERENCES user_image(user_image_id);
+
+--changeset alla:3
+ALTER TABLE ads
+    ADD FOREIGN KEY (user_id) REFERENCES users(user_id);
+ALTER TABLE ads
+    ADD FOREIGN KEY (ads_image_id) REFERENCES ads_image(ads_image_id);
+
+--changeset alla:4
+ALTER TABLE comment
+    ADD FOREIGN KEY (user_id) REFERENCES users(user_id);
+ALTER TABLE comment
+    ADD FOREIGN KEY (ads_id) REFERENCES ads(ads_id);
